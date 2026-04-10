6bb2128d3845dca44ae54c2b40a84c676d1d9209
Leandro Aguilar <aguilar41508493@gmail.com>
2026-04-10 00:23:28 -0300

    Se agregan las rutas, se arman los htmls de las vistas y se establece una primera barra de navegación

---
+++ resources/views/contacto.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="{{ asset('vendor/bootstrap/css/bootstrap.min.css') }}">
+    <title>Joyería</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Mi Sitio</a>
+                <div class="navbar-nav">
+                    <a class="nav-link" href="/home">Home</a>
+                    <a class="nav-link" href="/productos">Productos</a>
+                    <a class="nav-link active" href="/contacto">Contacto</a>
+                    <a class="nav-link" href="/nosotros">Nosotros</a>
+                </div>
+            </div>
+    </nav>
+
+
+
+
+    <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,20 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home function () {
+return view('home');
+})
+
+Route::get('/nosotros', function () {
+return view('nosotros');
+})
+
+Route::get('/productos', function () {
+return view('productos');
+})
+
+Route::get('/contacto', function () {
+return view('contacto');
+})
