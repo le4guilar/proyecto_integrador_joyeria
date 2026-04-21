bae9613b1e34c348b9b97aebe5ca893d6faf26d4
Leandro Aguilar <aguilar41508493@gmail.com>
2026-04-21 15:17:53 -0300

    Se agrega el formulario en contacto y se cambia el bg color del footer

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -70,10 +70,47 @@
         </div>
     </nav>
 
-    <main class="bg-red-custom text-center py-5">
+    <main class="container my-5 py-5">
+        <div class="text-center mb-5 mt-5">
+            <h6 class="text-uppercase mb-3" style="letter-spacing: 4px; color: #300403; font-weight: 600;">Servicios</h6>
+            <h2 style="font-family: 'Cormorant Garamond', serif;" class="display-5 text-dark">Contacto</h2>
+            <hr class="mx-auto" style="width: 60px; opacity: 0.2; color: #300403;">
+        </div>
 
+        <div class="container mt-5">
+            <div class="card p-4 border rounded-4 shadow-sm">
+                <form>
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="nombre" class="form-label">Nombre</label>
+                            <input type="text" class="form-control" id="nombre">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="email" class="form-label">Email:</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                    </div>
+                    
+                    <div class="row mb-3">
+                        <div class="col-md-6">
+                            <label for="apellido" class="form-label">Apellido:</label>
+                            <input type="text" class="form-control" id="apellido">
+                        </div>
+                        <div class="col-md-6">
+                            <label for="telefono" class="form-label">Teléfono:</label>
+                            <input type="tel" class="form-control" id="telefono">
+                        </div>
+                    </div>
 
-    </main>
+                    <div class="mb-3">
+                        <label for="consulta" class="form-label">Consulta:</label>
+                        <textarea class="form-control" id="consulta" rows="4"></textarea>
+                    </div>
+                    <button type="submit" class="btn btn-dark">Enviar</button>
+                </form>
+            </div>
+        </div>        
+    </main> 
 
     <footer class="bg-joyeria-footer pt-5 pb-4 container-fluid">
         <div class="container text-center text-md-start">
