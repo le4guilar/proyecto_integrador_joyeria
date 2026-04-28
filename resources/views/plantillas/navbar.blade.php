e80d5d2245e8b0269d2103b06bf2fa7beb38efd1
Leandro Aguilar <aguilar41508493@gmail.com>
2026-04-27 23:23:58 -0300

    hover al navbar

--- resources/views/plantillas/navbar.blade.php
+++ resources/views/plantillas/navbar.blade.php
@@ -1,49 +1,49 @@
 <nav class="navbar navbar-expand-lg navbar-dark navbar-transparent">
 
 
-    <!--botones-->   
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
-            <span class="navbar-toggler-icon">
-            </span>
-        </button>
+    <!--botones-->
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+        <span class="navbar-toggler-icon">
+        </span>
+    </button>
 
-        <div class="collapse navbar-collapse justify-content-center" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item">
-                    <a class="nav-link px-3" href="/home"> 
-                        ALBA
-                    </a>
-                </li>
+    <div class="nav-joyeria collapse navbar-collapse justify-content-center" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item">
+                <a class="nav-link px-3" href="/home">
+                    ALBA
+                </a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link px-3" href="/productos">
-                        Catálogo
-                    </a>
-                </li>
+            <li class="nav-item">
+                <a class="nav-link px-3" href="/productos">
+                    Catálogo
+                </a>
+            </li>
 
-                <li class="nav-item">
-                    <a class="nav-link px-3" href="/nosotros"> 
-                        Nosotros
-                    </a>
-                </li>
-                
-                <li class="nav-item">
-                    <a class="nav-link px-3" href="/comercializacion"> 
-                        Comercialización
-                    </a>
-                </li>
-            
-                <li class="nav-item">
-                    <a class="nav-link px-3" href="/terminos-de-uso"> 
-                        Términos de uso
-                    </a>
-                </li>
-                        
-                <li class="nav-item">
-                    <a class="nav-link px-3" href="/contacto">
-                        Contacto
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </nav>
+            <li class="nav-item">
+                <a class="nav-link px-3" href="/nosotros">
+                    Nosotros
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link px-3" href="/comercializacion">
+                    Comercialización
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link px-3" href="/terminos-de-uso">
+                    Términos de uso
+                </a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link px-3" href="/contacto">
+                    Contacto
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
