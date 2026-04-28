5e5c68d528203acad472a5231108a486fbb1c6df
Leandro Aguilar <aguilar41508493@gmail.com>
2026-04-27 21:43:22 -0300

    se corrige la ruta para el home

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactoController;
 
 Route::get('/', function () {
-    return view('patio');
+    return view('home');
 });
 
 Route::get('/home', function () {
