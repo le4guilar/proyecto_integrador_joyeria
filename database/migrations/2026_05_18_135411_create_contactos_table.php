526bb560e24f20a811bb33a7cf7d0de68f622b05
Leandro Aguilar <aguilar41508493@gmail.com>
2026-05-18 12:40:53 -0300

    se definen las tablas con las columnas, se agrega la columna rol como FK a la tabla usuario

---
+++ database/migrations/2026_05_18_135411_create_contactos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contactos', function (Blueprint $table) {
+            $table->id();
+            $table->string('nombre', 50);
+            $table->email('email', 50);
+            $table->string('telefono', 20);
+            $table->string('asunto', 50);
+            $table->string('mensaje', 500);
+            $table->boolean('estado');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contactos');
+    }
+};
