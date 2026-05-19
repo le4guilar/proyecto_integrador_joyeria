de9d46705869ec3b5745fbef099df62fa13a5afb
luci033 <luciidemetri09@gmail.com>
2026-05-19 20:28:38 -0300

    Se crea la tabla consulta

---
+++ database/migrations/2026_05_19_231853_create_consultas_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('consulta', function (Blueprint $table) {
+            $table->id();
+            $table->string('asunto' , 50);
+            $table->string('mensaje' , 200);
+            $table->foreignId('usuario_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('consulta');
+    }
+};
