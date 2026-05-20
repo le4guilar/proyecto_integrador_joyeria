0a59ff83d448634e3199b4448b4f0648bfac1230
Leandro Aguilar <aguilar41508493@gmail.com>
2026-05-20 20:20:47 -0300

    Se arreglan las relaciones y se mejora el modelo de BD

---
+++ database/migrations/2026_05_20_230541_asociar_domicilio_a_usuario.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('domicilio_id')->constrained('domicilio');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['domicilio_id']);
+            $table->dropColumn('domicilio_id');
+        });
+    }
+};
