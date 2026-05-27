0027279727782ce650409257b0938adf67cc8623
luci033 <luciidemetri09@gmail.com>
2026-05-26 21:01:37 -0300

    se modifica el modelo rol

--- app/Models/Rol.php
+++ app/Models/Rol.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Rol extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $table = 'rol';
+
+    protected $fillable = ['nombreRol'];
+
+
+    public function usuarios() {
+        return $this->hasMany(User::class, 'rol_id');
+    }
 }
