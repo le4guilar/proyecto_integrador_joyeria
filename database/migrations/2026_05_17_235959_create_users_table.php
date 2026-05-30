ee58b7ce9db8e62415f57d5984ebe24ced28e029
Leandro Aguilar <aguilar41508493@gmail.com>
2026-05-30 00:50:35 -0300

    Se establece un nuevo max para la columna password 4 caracteres

--- database/migrations/2026_05_17_235959_create_users_table.php
+++ database/migrations/2026_05_17_235959_create_users_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('apellido');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('password', 4);
 
             $table->foreignId('rol_id')->constrained('rol');
             
